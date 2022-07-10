72a420a882f315694de491883a34fc53ae5ee903
sheet00 <sheet00@rd5.so-net.ne.jp>
2022-07-10 11:40:25 +0900

    [fix] get_dht add retry

--- get_dht22.py
+++ get_dht22.py
@@ -7,9 +7,23 @@ def get_dht():
     # 接続したGPIOポート:22
     dhtDevice = adafruit_dht.DHT22(board.D22)
 
-    # 測定開始
-    t = dhtDevice.temperature
-    h = dhtDevice.humidity
+    #Simple test — Adafruit CircuitPython DHT Library 1.0 documentation https://docs.circuitpython.org/projects/dht/en/latest/examples.html#id1
+    is_success = False
+    while is_success == False:
+        try:
+            #測定開始
+            t = dhtDevice.temperature
+            h = dhtDevice.humidity
+            is_success = True
+
+        except RuntimeError as error:
+            # Errors happen fairly often, DHT's are hard to read, just keep going
+            print(error.args[0])
+            time.sleep(2.0)
+            continue
+        except Exception as error:
+            dhtDevice.exit()
+            raise error
 
     r = {"temp": t, "hum": h}
 
